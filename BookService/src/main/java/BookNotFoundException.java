b85e5ae9d32186a412c24821d3353d7447cec296
urbain nzouda <nzoudy@hotmail.fr>
2015-01-23 14:10:31 +0100

    Update BookNotFoundException.java
    formatage du code (getters et setters)

--- BookService/src/main/java/BookNotFoundException.java
+++ BookService/src/main/java/BookNotFoundException.java
@@ -7,21 +7,11 @@ public class BookNotFoundException extends Exception {
     private Long id;
     private String title;
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() {return id;}
 
-    public void setId(Long id) {
+    public void setId(Long id) {this.id = id;}
 
-        this.id = id;
-    }
+    public String getTitle() {return title;}
 
-    public String getTitle() {
-
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    public void setTitle(String title) {this.title = title;}
 }
